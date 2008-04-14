e55b473d2b1cb8571323fbd4aead93ad717bacec
Ingo Ruhnke <grumbel@gmx.de>
2008-04-15 00:06:38 +0200

    - updated the paypal link

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rsync --exclude-from=rsync-excludes.txt -rvtP . grumbel@pingus.seul.org:/home/grumbel/public_html/xboxdrv/
+rsync --exclude-from=rsync-excludes.txt --delete --delete-excluded -rvtP . grumbel@pingus.seul.org:/home/grumbel/public_html/xboxdrv/
 
 # EOF #
